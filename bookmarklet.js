f680ab683d7c9b6f6307a6c22e4c36b4ea4ce4f7
dialogik <dialogik@users.noreply.github.com>
2016-03-31 00:27:20 +0200

    Harmonize string delimiters
    When using it in HTML, e.g. <a href="...">, different string delimiters are obstructive.

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,8 +1,8 @@
 var url = window.location.href;
-if(url.substring(0, 19) != "https://twitter.com") {
+if(url.substring(0, 19) != 'https://twitter.com') {
     alert('This is not a twitter profile. Call this bookmarklet from twitter.com.');
 } else {
-	var username = url.split("/").pop();
-	var redirect = "http://twimes.io/u/" + username;
+	var username = url.split('/').pop();
+	var redirect = 'http://twimes.io/u/' + username;
 	window.location.href = redirect;
 }
